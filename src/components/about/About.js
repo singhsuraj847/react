60f949e35945ad4a610ca08ab11b4a706300436e
singhsuraj847 <kpt.singhsuraj1995@gmail.com>
2021-02-01 07:14:38 +0530

    initial

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,43 +1,52 @@
-import React from 'react'
-import {AboutContainer, AboutBox, AboutImg, AboutDetail, AboutTitle, AboutSection} from './About.style.js';
-import img2 from '../image/img2.jpg';
-import img3 from '../image/img3.jpg';
+import React from "react";
+import {
+  AboutContainer,
+  AboutBox,
+  AboutImg,
+  AboutDetail,
+  AboutSection,
+} from "./About.style.js";
+import img2 from "../image/img2.jpg";
+import img3 from "../image/img3.jpg";
 
 function About() {
-    return (
-        <AboutSection>
-            <h1>About US</h1>
-        <AboutContainer>
-            <AboutBox>
-                <AboutImg>
-                    <img src={img2}/>
-                </AboutImg>
-                <AboutDetail>
-                    <h2>Suraj Singh</h2><br/>
-                    <span>Web Designer</span>
-                </AboutDetail>
-            </AboutBox>
-            <AboutBox>
-                <AboutImg>
-                    <img src={img3}/>
-                </AboutImg>
-                <AboutDetail>
-                    <h2>Karan Malhotra</h2><br/>
-                    <span>Web Designer</span>
-                </AboutDetail>
-            </AboutBox>
-            <AboutBox>
-                <AboutImg>
-                    <img src={img2}/>
-                </AboutImg>
-                <AboutDetail>
-                    <h2>Suraj Singh</h2><br/>
-                    <span>Web Designer</span>
-                </AboutDetail>
-            </AboutBox>
-        </AboutContainer>
-        </AboutSection>
-    )
+  return (
+    <AboutSection>
+      <h1>About US</h1>
+      <AboutContainer>
+        <AboutBox>
+          <AboutImg>
+            <img src={img2} />
+          </AboutImg>
+          <AboutDetail>
+            <h2>Suraj Singh</h2>
+            <br />
+            <span>Web Designer</span>
+          </AboutDetail>
+        </AboutBox>
+        <AboutBox>
+          <AboutImg>
+            <img src={img3} />
+          </AboutImg>
+          <AboutDetail>
+            <h2>Karan Malhotra</h2>
+            <br />
+            <span>Web Designer</span>
+          </AboutDetail>
+        </AboutBox>
+        <AboutBox>
+          <AboutImg>
+            <img src={img2} />
+          </AboutImg>
+          <AboutDetail>
+            <h2>Suraj Singh</h2>
+            <br />
+            <span>Web Designer</span>
+          </AboutDetail>
+        </AboutBox>
+      </AboutContainer>
+    </AboutSection>
+  );
 }
 
-export default About
+export default About;
